481e5435f0a3a49ca9b303590b547feb57811ccb
YangGeumhee <rmagml9769@gmail.com>
2024-07-19 22:55:58 +0900

    feat : 할인, 주문 기능 추가

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,23 @@
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+// 아래와 같이 스프링이나 컨테이너의 도움 없이 순수 자바 코드로만 이루어진 단위 테스트를 잘 짜는 것이 중요하다.
+public class OrderServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
