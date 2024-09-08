4f5968b8debb1375ce470ecc3e879bcdbe2e463c
geumhee <rmagml9769@gmail.com>
2024-09-08 15:32:57 +0900

    인터페이스 구현한 정률 정책 기능 추가

--- src/main/java/hello/core/discount/DiscountPolicy.java
+++ src/main/java/hello/core/discount/DiscountPolicy.java
@@ -1,5 +1,6 @@
 package hello.core.discount;
 
+
 import hello.core.member.Member;
 
 public interface DiscountPolicy {
@@ -7,5 +8,6 @@ public interface DiscountPolicy {
      *
      * @return 할인 대상 금액
      */
+
     int discount(Member member, int price);
 }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,15 +2,25 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService{
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // 클라이언트인 'OrderServiceImpl'이 'DiscountPolicy' 인터페이스 뿐만 아니라 FixDiscountPolicy 인
+//    구체 클래스도 함께 의존하고 있다. DIP(Dependency Inversion Principle) 위반
 
+    // 그래서 FixDiscountPolicy를 RateDiscountPolicy로 변경하는 순간 OrderServiceImpl 의
+    // 소스 코드도 함께 변경해야 한다. OCP(open close principle) 위반
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private DiscountPolicy discountPolicy;
+//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+//    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    //단일 책임의 원칙 잘 지킴. 할인과 주문을 분리
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
